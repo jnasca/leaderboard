39930bc22eb97186dddc20d8cc43ebc6c176d3c9
jnasca <jnasca221@gmail.com>
2016-10-11 10:05:05 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -3,4 +3,29 @@
  * Created by Joseph on 10/11/2016.
  */
 
-console.log("Hello world");
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers ({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+
+if(Meteor.isServer){
+
+}
